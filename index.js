5e5972c2d3fcf37a36b7db75c754ffd5adeaa708
Bea Go <beanicolego@gmail.com>
2018-05-05 22:58:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart = ['fart'];
+var cart = [];
 
 function getCart() {
  return cart;
@@ -47,7 +47,12 @@ function total() {
 
 function removeFromCart(item) {
   for ( let i=0; i < cart.length; i++) {
-    if (item === )
+    if (item === cart[i].itemName) {
+      cart.splice(cart[i]);
+      return cart;
+    } else {
+      return `That item is not in your cart.`;
+    }
   }
 }
 
